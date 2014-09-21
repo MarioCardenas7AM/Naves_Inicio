ad3e4fe7dfab89301370a64ea79aae8bf2d5f166
Mario Alberto cardenas singh <themkrds@gmail.com>
2014-09-21 12:30:22 -0700

    Actitivad 2
    Agregando motor de estados
    
    NOTA:al momento de hacer la practica me marcaba error en una parte y no
    me dejaba compilar
    
    CGame::CGame(){
    estado = 0;
    
    tuve que cambiarlo de esta manera
    CGame::CGame(){
    estado = estado;
    
    no se que hice y no se si este bien pero me marcaba error y no me dejaba
    compilar
    espero que me diga que hice porque solo segui al espiritu santo.

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -3,7 +3,7 @@
 #include "Game.h"
 
 CGame::CGame(){
-	estado = 0;
+	estado = estado;
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
@@ -19,12 +19,23 @@ bool CGame::Start()
             
 		//Maquina de estados
 		switch(estado){
-			case 0: //INICIALIZAR
+		case  Estado::ESTADO_INICIANDO:
+		
 			break;
-			case 1:	//JUGAR	
+		case   Estado::ESTADO_MENU:
+			
 			break;
-			case 2: //SALIR
-				salirJuego = true;
+		case  Estado::ESTADO_JUGANDO:
+				
+				
+			break;
+		case Estado::ESTADO_TERMINANDO:
+			
+			break;
+		case Estado::ESTADO_FINALIZANDO:
+			salirJuego = true;
+			
+			
 			break;
 		};
     }
--- Source/Game.h
+++ Source/Game.h
@@ -2,12 +2,23 @@
 class CGame
 {
 public:
+	
 	bool Start();
 	static CGame instanceGame;
 
 	CGame();
 	void Finalize();
 
+	enum Estado{
+		ESTADO_INICIANDO,
+		ESTADO_MENU,
+		ESTADO_JUGANDO,
+		ESTADO_TERMINANDO,
+		ESTADO_FINALIZANDO
+		
+	};
+	
+
 private:
-	int estado;
+	Estado estado;
 };
